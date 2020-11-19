2b9301fead382ef594eb37726988ff56ade6b2f7
yenikolisnichenko <yenikolisnichenko@gmail.com>
2020-11-19 02:03:14 -0500

    adding timestamp func, only works with double digits

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/csv"
 	"log"
 	"os"
+	"time"
 )
 
 func main() {
@@ -44,6 +45,38 @@ func writeCSV(lines [][]string) {
 	defer writer.Flush()
 
 	for _, line := range lines {
-		_ = writer.Write(line)
+
+		timeStamp := changeTime(line[0])
+		address := line[1]
+		comment := line[7]
+
+		normLine := []string{
+			timeStamp,
+			address,
+			comment,
+		}
+
+		_ = writer.Write(normLine)
 	}
+}
+
+// changeTime takes in a timestamp and converts it to RFC3339 and EST timezone
+func changeTime(timestamp string) string {
+
+	const format = "01/02/06 03:04:05 PM"
+
+	// get current timezone
+	PST, _ := time.LoadLocation("America/Los_Angeles")
+
+	// get desired timezone
+	EST, _ := time.LoadLocation("America/New_York")
+
+	t, err := time.ParseInLocation(format, timestamp, PST)
+	if err != nil {
+		log.Fatal("Error parsing time in location: ", err)
+	}
+	// get timestamp in RFC3339 format and EST timezone
+	t.Format(time.RFC3339)
+	estTime := t.In(EST)
+	return estTime.String()
 }
